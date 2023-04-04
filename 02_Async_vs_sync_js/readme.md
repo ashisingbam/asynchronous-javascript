9f29bb9a3a04fb0f43f44098ce47b3b87d05b6ae
ashisingbam <ashisinghbam@example.com>
2023-04-04 17:36:48 +0530

    async vs sync complete

---
+++ 02_Async_vs_sync_js/readme.md
@@ -0,0 +1,19 @@
+## Slow API Link:https://6wrlmkp9u2.execute-api.us-east-1.amazonaws.com/?sleep=5000
+
+- 00:00 - Introduction
+- 00:44 - What is a thread exactly? Thread in JavaScript
+- 04:55 - How JavaScript is single-threaded? With examples
+- 07:35 - Watch "Event Loop and Callback Queue in JavaScript"
+- 08:08 - Asynchronous Code vs Synchronous Code
+- 11:05 - How can we block the main thread - the easy way?
+- 14:12 - How to block the main thread using while loop?
+- 14:38 - What is Date.now()?
+- 17:06 - Blocking the main thread - Continued
+- 25:36 - Quick Summary
+- 27:41 - How asynchronous code works?
+- 44:40 - What is AJAX?
+- 47:13 - Pros of AJAX
+- 47:58 - Conclusion
+
+
+
---
+++ 02_Async_vs_sync_js/script.js
@@ -0,0 +1,45 @@
+const image = document.querySelector("img");
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+  // fetch("https://dog.ceo/api/breeds/image/random")
+  //   .then((response) => response.json())
+  //   .then((json) => {
+  //     console.log("got the data");
+  //     console.log(json);
+  //     image.src = json.message;
+  //   });
+  const xhr = new XMLHttpRequest();
+  console.log(xhr);
+  xhr.responseType = "json";
+  // xhr.addEventListener("load", () => {
+  //   console.log(xhr.response);
+  //   image.src = xhr.response.message;
+  // });
+  xhr.onload = () => {
+    console.log(xhr.response);
+    image.src = xhr.response.message;
+  }
+  xhr.open("GET", "https://dog.ceo/api/breeds/image/random");
+  xhr.send();
+});
+
+// setTimeout(() => {
+//   console.log('hi');
+// }, 4000)
+
+// const blockingBtn = document.querySelector('.blocking-btn')
+
+// blockingBtn.addEventListener('click', ()=> {
+//   const startTime = Date.now()
+//   let currentTime = startTime
+  
+//   while(startTime + 2000 > currentTime) {
+//     console.log('current');
+//     currentTime = Date.now()
+//   }
+// })
+
+
+
+
