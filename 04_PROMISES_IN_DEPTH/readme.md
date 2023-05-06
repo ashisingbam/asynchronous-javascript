59d447a2bfc5f417f5c7b2c4a18f6d061d0cbd41
ashisingbam <ashisinghbam@example.com>
2023-05-07 00:01:22 +0530

    promises in depth complete

---
+++ 04_PROMISES_IN_DEPTH/readme.md
@@ -0,0 +1,14 @@
+## Promises Explained in Depth
+
+- 00:00 - Introduction
+- 00:51 - What is a Promise?
+- 03:48 - States of Promise - Resolve, Reject, and Pending
+- 07:35 - Change Promise State on Click on a Button
+- 10:27 - Understand .then(), .catch() method
+- 16:11 - Quick Recap
+- 17:47 - Behind the Scene - how .then() and .catch() works?
+- 21:08 - When .finally() method is called?
+- 22:46 - How Promise Works Behind the Scene - The Microtask and Callback Queue?
+- 37:43 - Understand When Promise is Settled!
+- 39:40 - What does .then() returns?
+- 46:25 - Conclusion
---
+++ 04_PROMISES_IN_DEPTH/script.js
@@ -0,0 +1,34 @@
+const resolveBtn = document.querySelector('#resolve-btn')
+const rejectBtn = document.querySelector('#reject-btn')
+
+const p = new Promise((resolve, reject) => {
+  resolveBtn.addEventListener('click', () => {
+    resolve('promise resolveddd')
+  })
+  rejectBtn.addEventListener('click', () => {
+    reject('promise rejeeeected')
+  })
+})
+
+const p2 = p.then((data) => {
+  console.log(data);
+  return 155
+})
+p2.then((data) => {
+  console.log(data);
+  return 30
+}).then((data) => {
+  console.log(data);
+}).catch((err) => {
+  console.log(err);
+  return 40
+}).then((data) => {
+  console.log(data);
+})
+// }).catch((err) => {
+//   console.log(err);
+// })
+
+// setTimeout(() => {
+//   console.log('times up');
+// })
